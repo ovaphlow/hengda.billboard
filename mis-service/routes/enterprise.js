b3b4060079fa884ebfa0dc730b9ea4f81bfacdba
Ovaphlow <ovaphlow@outlook.com>
2020-02-09 10:47:12 +0800

    企业用户

--- mis-service/routes/enterprise.js
+++ mis-service/routes/enterprise.js
@@ -9,6 +9,77 @@ const router = new Router({
 module.exports = router
 
 router
+  .get('/:id/user/:user_id', async ctx => {
+    const sql = `
+      select id, enterprise_id, username, name
+      from enterprise_user
+      where id = ? and enterprise_id = ?
+      limit 1
+    `
+    const pool = mysql.promise()
+    try {
+      const [rows, fields] = await pool.query(sql, [ctx.params.user_id, ctx.params.id])
+      ctx.response.body = { message: '', content: rows.length === 1 ? rows[0]: {} }
+    } catch (err) {
+      console.error(err)
+      ctx.response.body = { message: '服务器错误', content: '' }
+    }
+  })
+  .put('/:id/user/:user_id', async ctx => {
+    const sql = `
+      update enterprise_user
+      set username = ?, name = ?
+      where id = ? and enterprise_id = ?
+    `
+    const pool = mysql.promise()
+    try {
+      await pool.execute(sql, [
+        ctx.request.body.username,
+        ctx.request.body.name,
+        ctx.params.user_id,
+        ctx.params.id
+      ])
+      ctx.response.body = { message: '', content: '' }
+    } catch (err) {
+      console.error(err)
+      ctx.response.body = { message: '服务器错误', content: '' }
+    }
+  })
+  .get('/:id/user/', async ctx => {
+    const sql = `
+      select id, enterprise_id, username, name
+      from enterprise_user
+      where enterprise_id = ?
+    `
+    const pool = mysql.promise()
+    try {
+      const [rows, fields] = await pool.query(sql, [ctx.params.id])
+      ctx.response.body = { message: '', content: rows }
+    } catch (err) {
+      console.error(err)
+      ctx.response.body = { message: '服务器错误', content: '' }
+    }
+  })
+  .post('/:id/user/', async ctx => {
+    const sql = `
+      insert into enterprise_user (enterprise_id, username, name)
+      values (?, ?, ?)
+    `
+    const pool = mysql.promise()
+    try {
+      await pool.execute(sql, [
+        ctx.params.id,
+        ctx.request.body.username,
+        ctx.request.body.name
+      ])
+      ctx.response.body = { message: '', content: '' }
+    } catch (err) {
+      console.error(err)
+      ctx.response.body = { message: '服务器错误', content: '' }
+    }
+  })
+
+router
   .get('/:id', async ctx => {
     const sql = `
       select * from enterprise where id = ? limit 1
--- mis-webapp/src/Enterprise.js
+++ mis-webapp/src/Enterprise.js
@@ -11,6 +11,8 @@ export default function EnterpriseRouter() {
         <Route exact path="/企业"><List /></Route>
         <Route exact path="/企业/新增"><Detail category="新增" /></Route>
         <Route exact path="/企业/:id"><Detail category="编辑" /></Route>
+        <Route path="/企业/:id/新增用户"><UserDetail category="新增" /></Route>
+        <Route path="/企业/:id/编辑用户/:user_id"><UserDetail category="编辑" /></Route>
       </Switch>
     </Router>
   )
@@ -117,6 +119,7 @@ function List() {
 }
 
 function Detail(props) {
+  const { id } = useParams()
   const [data, setData] = useState({
     id: 0,
     name: '',
@@ -126,7 +129,7 @@ function Detail(props) {
     zhiziguimo: '',
     yuangongshuliang: ''
   })
-  const { id } = useParams()
+  const [dataUserList, setDataUserList] = useState([])
 
   useEffect(() => {
     if (props.category === '编辑') {
@@ -138,6 +141,22 @@ function Detail(props) {
           return
         }
         setData(res.content)
+      }
+      fetchData(id)
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  useEffect(() => {
+    if (props.category === '编辑') {
+      const fetchData = async id => {
+        const response = await fetch(`/api/enterprise/${id}/user/`)
+        const res = await response.json()
+        if (res.message) {
+          window.console.error(res.message)
+          return
+        }
+        setDataUserList(res.content)
       }
       fetchData(id)
     }
@@ -192,31 +211,167 @@ function Detail(props) {
             <h3>{props.category} 企业</h3>
             <hr />
 
-            <div className="card shadow">
-              <div className="card-body">
-                <TextRowField caption="名称" name="name" value={data.name || ''} handleChange={handleChange} />
+            <div className="row">
+              <div className="col-8">
+                <div className="card shadow">
+                  <div className="card-header">
+                    企业信息
+                  </div>
 
-                <TextRowField caption="营业执照" name="yingyezhizhao" value={data.yingyezhizhao || ''} handleChange={handleChange} />
+                  <div className="card-body">
+                    <TextRowField caption="名称" name="name" value={data.name || ''} handleChange={handleChange} />
 
-                <TextRowField caption="法人" name="faren" value={data.faren || ''} handleChange={handleChange} />
+                    <TextRowField caption="营业执照" name="yingyezhizhao" value={data.yingyezhizhao || ''} handleChange={handleChange} />
 
-                <TextRowField caption="注册日期" name="zhuceriqi" value={data.zhuceriqi || ''} handleChange={handleChange} />
+                    <TextRowField caption="法人" name="faren" value={data.faren || ''} handleChange={handleChange} />
 
-                <TextRowField caption="注册规模" name="zhuziguimo" value={data.zhuziguimo || ''} handleChange={handleChange} />
+                    <TextRowField caption="注册日期" name="zhuceriqi" value={data.zhuceriqi || ''} handleChange={handleChange} />
 
-                <div className="form-group row">
-                  <label className="col-sm-2 col-form-label text-right">员工数量</label>
-                  <div className="col-sm-10">
-                    <select name="yuangongshuliang" value={data.yuangongshuliang} className="form-control" onChange={handleChange}>
-                      <option value="未选择">未选择</option>
-                      {
-                        YUAN_GONG_SHU_LIANG.map((it, index) => (
-                          <option value={it} key={index}>{it}</option>
-                        ))
-                      }
-                    </select>
+                    <TextRowField caption="注册规模" name="zhuziguimo" value={data.zhuziguimo || ''} handleChange={handleChange} />
+
+                    <div className="form-group row">
+                      <label className="col-sm-2 col-form-label text-right">员工数量</label>
+                      <div className="col-sm-10">
+                        <select name="yuangongshuliang" value={data.yuangongshuliang} className="form-control" onChange={handleChange}>
+                          <option value="未选择">未选择</option>
+                          {
+                            YUAN_GONG_SHU_LIANG.map((it, index) => (
+                              <option value={it} key={index}>{it}</option>
+                            ))
+                          }
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="card-footer">
+                    <div className="btn-group">
+                      <BackwardButton />
+                    </div>
+
+                    <div className="btn-group pull-right">
+                      <button type="button" className="btn btn-primary" onClick={handleSubmit}>
+                        <i className="fa fa-fw fa-check"></i>
+                        确定
+                      </button>
+                    </div>
                   </div>
                 </div>
+              </div>
+
+              <div className="col-4">
+                <div className="card shadow">
+                  <div className="card-header">
+                    企业用户
+                    <div className="btn-group pull-right">
+                      <button type="button" className="btn btn-sm btn-outline-success"
+                        onClick={() => window.location = `#企业/${id}/新增用户`}
+                      >
+                        添加用户
+                      </button>
+                    </div>
+                  </div>
+
+                  <div className="card-body">
+                    <div className="list-group">
+                      {
+                        dataUserList.map(it => (
+                          <a href={`#企业/${id}/编辑用户/${it.id}`} className="list-group-item list-group-item-action" key={it.id}>
+                            {it.name}
+                            <span className="pull-right text-muted">{it.username}</span>
+                          </a>
+                        ))
+                      }
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+function UserDetail(props) {
+  const { id, user_id } = useParams()
+  const [data, setData] = useState({
+    id: 0,
+    enterprise_id: 0,
+    username: '',
+    name: ''
+  })
+
+  useEffect(() => {
+    if (props.category === '编辑') {
+      const fetchData = async (id, user_id) => {
+        const response = await fetch(`/api/enterprise/${id}/user/${user_id}`)
+        const res = await response.json()
+        if (res.message) {
+          window.console.error(res.message)
+          return
+        }
+        setData(res.content)
+      }
+      fetchData(id, user_id)
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const handleChange = e => {
+    const { value, name } = e.target
+    setData(prev => ({ ...prev, [name]: value}))
+  }
+
+  const handleSubmit = async () => {
+    if (props.category === '新增') {
+      const response = await window.fetch(`/api/enterprise/${id}/user/`, {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(data)
+      })
+      const res = await response.json()
+      if (res.message) {
+        window.alert(res.message)
+        return
+      }
+      window.location = `#企业/${id}`
+    } else if (props.category === '编辑') {
+      const response = await window.fetch(`/api/enterprise/${id}/user/${user_id}`, {
+        method: 'PUT',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(data)
+      })
+      const res = await response.json()
+      if (res.message) {
+        window.alert(res.message)
+        return
+      }
+      window.location = `#企业/${id}`
+    }
+  }
+
+  return (
+    <>
+      <Title />
+      <Navbar />
+
+      <div className="container-fluid mt-3">
+        <div className="row">
+          <div className="col-3 col-lg-2">
+            <SideNav />
+          </div>
+
+          <div className="col-9 col-lg-10">
+            <h3>{props.category} 企业用户</h3>
+            <hr />
+
+            <div className="card shadow">
+              <div className="card-body">
+                <TextRowField caption="姓名" name="name" value={data.name || ''} handleChange={handleChange} />
+
+                <TextRowField caption="用户名" name="username" value={data.username || ''} handleChange={handleChange} />
               </div>
 
               <div className="card-footer">
@@ -226,7 +381,7 @@ function Detail(props) {
 
                 <div className="btn-group pull-right">
                   <button type="button" className="btn btn-primary" onClick={handleSubmit}>
-                    <i className="fa fa-fw fa-check"></i>
+                    <i className="fa fa-fw fa-edit"></i>
                     确定
                   </button>
                 </div>

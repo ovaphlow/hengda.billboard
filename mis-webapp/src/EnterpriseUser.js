47ccf8219301131a86f1c45e8b9fdd2989050ea2
ovaphlow <ovaphlow@outlook.com>
2020-04-16 16:12:03 +0800

    close #40

--- mis-webapp/src/EnterpriseUser.js
+++ mis-webapp/src/EnterpriseUser.js
@@ -23,6 +23,36 @@ export default function EnterpriseUserRouter() {
   )
 }
 
+export function List(props) {
+  const [data_list, setDataList] = useState([])
+
+  useEffect(() => {
+    ;(async enterprise_id => {
+      const response = await fetch(`/api/enterprise/${enterprise_id}/user/`)
+      const res = await response.json()
+      if (res.message) {
+        window.console.error(res.message)
+        return
+      }
+      setDataList(res.content)
+    })(props.enterprise_id)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <div className="list-group">
+      {
+        data_list.map(it => (
+          <a href={`#企业用户/${it.id}?uuid=${it.uuid}&enterprise_id=${props.enterprise_id}&enterprise_uuid=${props.enterprise_uuid}`} className="list-group-item list-group-item-action" key={it.id}>
+            {it.name}
+            <span className="pull-right text-muted">{it.username}</span>
+          </a>
+        ))
+      }
+    </div>
+  )
+}
+
 function Detail(props) {
   const { id } = useParams()
   const location = useLocation()

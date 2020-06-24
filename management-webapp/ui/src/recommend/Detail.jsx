924ab7f18cdf148cb8bedad4ecf9b2d8f76fb687
ovaphlow <ovaphlow@live.com>
2020-06-24 08:56:34 +0800

    ...

--- management-webapp/ui/src/recommend/Detail.jsx
+++ management-webapp/ui/src/recommend/Detail.jsx
@@ -6,7 +6,6 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 import Navbar from '../component/Navbar';
-import Toolbar from './ComponentToolbar';
 import { RECOMMEND_CATEGORY } from '../constant';
 import { useAddressKeys, useAddressValues, useAddressLevel1ValueList } from '../useAddress';
 
@@ -130,16 +129,23 @@ export default function Detail({ cat }) {
     <>
       <Navbar category="推荐信息" />
 
-      <div className="container mt-3 mb-5">
-        <h3>
-          {cat}
-          {' '}
-          推荐信息
-        </h3>
-        <hr />
+      <div className="container-fluid">
+        <nav aria-label="breadcrumb">
+          <h1>
+            <ol className="breadcrumb bg-dark">
+              <li className="breadcrumb-item">
+                <a href="#/" className="text-reset text-decoration-none">推荐信息</a>
+              </li>
+              <li className="breadcrumb-item active">{cat}</li>
+            </ol>
+          </h1>
+        </nav>
+        <div className="p-2" />
+      </div>
 
-        <Toolbar />
+      <div className="m-5" />
 
+      <div className="container">
         <div className="card bg-dark shadow">
           <div className="card-body">
             <div className="row">
--- management-webapp/ui/src/recommend/List.jsx
+++ management-webapp/ui/src/recommend/List.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
 import Navbar from '../component/Navbar';
-import Toolbar from './ComponentToolbar';
 
 export default function List() {
   const [list, setList] = useState([]);
@@ -18,11 +17,28 @@ export default function List() {
     <>
       <Navbar category="推荐信息" />
 
-      <div className="container mt-3 mb-5">
-        <h3>推荐信息</h3>
-        <hr />
+      <div className="container-fluid">
+        <nav aria-label="breadcrumb">
+          <h1>
+            <ol className="breadcrumb bg-dark">
+              <li className="breadcrumb-item active">推荐信息</li>
+            </ol>
+          </h1>
+        </nav>
+        <div className="p-2" />
+      </div>
 
-        <Toolbar />
+      <div className="m-5" />
+
+      <div className="container">
+        <div className="btn-group">
+          <a href="#/新增" className="btn btn-sm btn-light">
+            <i className="fa fa-fw fa-plus" />
+            新增
+          </a>
+        </div>
+
+        <div className="m-2" />
 
         <div className="card bg-dark shadow">
           <div className="card-body">

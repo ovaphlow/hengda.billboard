49755166c1f645f3453d9ad2d985d6c0e7b5b320
ovaphlow <ovaphlow@live.com>
2020-07-13 09:05:55 +0800

    更新推荐信息分类

--- management-webapp/ui/.eslintrc.js
+++ management-webapp/ui/.eslintrc.js
@@ -1,26 +1,26 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2020": true
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "plugin:react/recommended",
-        "airbnb"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "react-hooks",
-    ],
-    "rules": {
-        "camelcase": 0,
-        "no-alert": 0,
-        "jsx-a11y/label-has-associated-control": 0,
-    }
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+  ],
+  rules: {
+    camelcase: 0,
+    'no-alert': 0,
+    'jsx-a11y/label-has-associated-control': 0,
+  },
 };

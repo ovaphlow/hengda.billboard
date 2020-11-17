bd0cf70dfaacaf284b8f80772840f5c5b8d0baf7
ovaphlow <ovaphlow@live.com>
2020-11-17 14:57:01 +0800

    变更http服务启动形式

--- dispatcher/app.js
+++ dispatcher/app.js
@@ -1,5 +1,7 @@
-const Koa = require('koa');
+const cluster = require('cluster');
+const http = require('http');
 
+const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 
 const config = require('./config');
@@ -165,3 +167,33 @@ app.on('error', (err, ctx) => {
 })();
 
 module.exports = app;
+
+if (require.main === module) {
+  if (cluster.isMaster) {
+    logger.log(`${new Date()} 主进程 PID:${process.pid}`);
+
+    for (let i = 0; i < config.app.numChildProcesses; i += 1) {
+      cluster.fork();
+    }
+
+    cluster.on('online', (worker) => {
+      logger.log(
+        `${new Date()} 子进程 PID:${worker.process.pid}, 端口:${
+          config.app.port
+        }`,
+      );
+    });
+
+    cluster.on('exit', (worker, code, signal) => {
+      logger.log(
+        `${new Date()} 子进程 PID:${
+          worker.process.pid
+        }终止，错误代码:${code}，信号:${signal}`,
+      );
+      logger.log(`${new Date()} 由主进程(PID:${process.pid})创建新的子进程`);
+      cluster.fork();
+    });
+  } else {
+    http.createServer(app.callback()).listen(config.app.port);
+  }
+}

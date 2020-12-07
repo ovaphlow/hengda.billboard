24f468909494d400f1a8f932ee2806e8df5a61b1
ovaphlow <ovaphlow@live.com>
2020-12-07 14:56:30 +0800

    remove package-lock.json

--- ent-webapp/src/setupProxy.js
+++ ent-webapp/src/setupProxy.js
@@ -1,13 +1,15 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+module.exports = (app) => {
+  app.use(
+    createProxyMiddleware('/api', {
+      target: 'http://192.168.1.248:6000',
+    }),
+  );
+};
+
 // module.exports = (app) => {
 //   app.use(createProxyMiddleware('/api', {
-//     target: 'http://127.0.0.1:6000',
+//     target: 'http://211.159.150.3',
 //   }));
 // };
-
-module.exports = (app) => {
-  app.use(createProxyMiddleware('/api', {
-    target: 'http://211.159.150.3',
-  }));
-};
